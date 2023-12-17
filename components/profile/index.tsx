ee7d20b4d2fea659f614cb1c22d4fefb2a009b26
Bruno Ken <bruno_am_22@hotmail.com>
2023-12-16 21:26:25 -0300

    repareir key profileComp

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -29,6 +29,7 @@ export function ProfileComponent() {
         <div className='flex gap-8'>
           {buttonEditUser.map((nav) => (
             <Link
+              key={nav.text}
               href={nav.link}
               className='border-[1px] border-primary p-2 hover:opacity-70 font-medium rounded-lg flex gap-2'>
               {nav.svg} {nav.text}
